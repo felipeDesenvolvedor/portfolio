a1d58f05576957d5867c6712f6ceb9c99cfef05f
felipeDesenvolvedor <46979397+felipeDesenvolvedor@users.noreply.github.com>
2021-09-11 10:06:17 -0300

    Estrutura inicial

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once("head.php")?>
+
+<body>
+    <?php require_once("header/header.php")?>
+    <?php require_once("main/main.php")?>
+    <?php require_once("footer/footer.php")?>
+</body>
+</html>
